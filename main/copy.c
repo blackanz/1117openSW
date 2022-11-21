a682d6a07c0b6bf0471c43c33b787e1cceb66e97
blackanz <118209666+blackanz@users.noreply.github.com>
2022-11-22 00:48:54 +0900

    Update copy.c

--- main/copy.c
+++ main/copy.c
@@ -5,7 +5,7 @@ char* copy(char * origin)
 {
 	int len = strlen(origin) + 1;
 	char* dest = malloc(sizeof(char)*len);
-	for(int i=0; i<strlen(origin); i++)
+	for(int i=0; i < strlen(origin); i++)
 	{
 		dest[i] = origin[i];
 	}
